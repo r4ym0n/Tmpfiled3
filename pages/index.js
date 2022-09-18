5e8ae19c09d837a3986018835a4eebf38b83e1f2
quartz010 <quartz010@outlook.com>
2022-09-19 03:18:34 +0800

    fix padding

--- pages/index.js
+++ pages/index.js
@@ -66,7 +66,7 @@ export default function Home() {
         }
 
         main {
-          padding: 3rem 0;
+          padding: 1rem 0;
           flex: 1;
           display: flex;
           flex-direction: column;
